15f0872d5ca165013faa04390e2035dfd714e6b6
Otec <oleg.kuzembaev@gmail.com>
2022-01-16 22:05:48 +0700

    Adapter was made

---
+++ app/src/main/java/com/example/shoppinglistapp/presentation/ShopListAdaper.kt
@@ -0,0 +1,44 @@
+package com.example.shoppinglistapp.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shoppinglistapp.R
+import com.example.shoppinglistapp.domain.ShopItem
+
+class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+
+    var shopList = listOf<ShopItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    class ShopItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvName = view.findViewById<TextView>(R.id.tv_name)
+        val tvCount = view.findViewById<TextView>(R.id.tv_count)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_shop_disabled,
+            parent,
+            false
+        )
+        return ShopItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHoler: ShopItemViewHolder, position: Int) {
+        val shopItem = shopList[position]
+        viewHoler.tvName.text = shopItem.name
+        viewHoler.tvCount.text = shopItem.count.toString()
+    }
+
+
+
+    override fun getItemCount(): Int {
+        return shopList.size
+    }
+}
