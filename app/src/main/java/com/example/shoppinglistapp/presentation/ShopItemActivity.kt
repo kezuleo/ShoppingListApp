6822c40ea48aec0430e9e31372e00511e18866f7
Otec <oleg.kuzembaev@gmail.com>
2022-01-23 20:37:20 +0700

    Shop_item_activity layout was updated

--- app/src/main/java/com/example/shoppinglistapp/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/shoppinglistapp/presentation/ShopItemActivity.kt
@@ -1,10 +1,13 @@
-package com.sumin.shoppinglist.presentation
+package com.example.shoppinglistapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.shoppinglistapp.R
 
 class ShopItemActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: ShopItemViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
